33ea42d23ef0b0310e09e00e07e967e9a75c4aa1
luis.teixeira <luis.teixeira@gmail.com>
2022-03-25 21:28:39 -0300

    Removing unused dto

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { User, UserDocument} from './schema/user.schema';
 
 @Injectable()
